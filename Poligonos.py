5ceb9c9d0f5fea2f58a71a0f4fd1d346f50d30ab
lierniortiz <65980093+lierniortiz@users.noreply.github.com>
2020-06-01 20:23:21 +0200

    Update Poligonos.py

--- Poligonos.py
+++ Poligonos.py
@@ -1,14 +1,11 @@
 
 
-def poligono(lista,numero_lados,longitud_lados):
+def poligono(lista,numero_lados,longitud_lados,n):
     suma_lados=0
     p=numero_lados
-    for i in range(len(lista)):
-        #print("iterazioa",i)
-        for j in range(i-len(lista),i):
+    for i in range(n):
+        for j in range(i-n,i):
             suma_lados=suma_lados+lista[j]
-            #print("Lista[j]",lista[j])
-            #print(suma_lados)
             if suma_lados>longitud_lados:
                 suma_lados=0
                 break
@@ -18,49 +15,22 @@ def poligono(lista,numero_lados,longitud_lados):
                 suma_lados=0
             if p==0:
                 return True
+    return -1
 
 
 n=int(input())
 while n!=0:
-    #lst1 = [int(item) for item in input("Enter the list items : ").split()]
     lista = list(map(int,input().strip().split()))[:n]
     s=sum(lista)
-    for i in range(s//2,2,-1):
-        if s%i==0 and n>=i:
-            #print(i)
+    for i in range(n,2,-1):
+        if s%i==0:
             numero_lados=i
             longitud_arco=s/i
-            #print("numero de lados:",numero_lados)
-            #print("longitud_arco:",longitud_arco)
-            if poligono(lista,numero_lados,longitud_arco)==True:
+            if poligono(lista,numero_lados,longitud_arco,n)==True:
                 print(n-i)
-                #TIENE QUE SALIR DE AQUÍ DESPUÉS DE IMPRIMIIIIIR!!!!!!!!!!!!!!!
-            elif i==3:
-                print(-1)
+                break
+    else:
+        print (poligono(lista,numero_lados,longitud_arco,n))
                 
             
     n=int(input())
-                    
-                
-
-
-
-
-    
-          
-        
-#print(poligono([2,2,3,5,1,4,1],5,4))
-
-    #Como máximo vamos a tener un conjunto de n-numero lados-1
-    
-    
-
-
-
-
-
-
-
-    
-    
-    
